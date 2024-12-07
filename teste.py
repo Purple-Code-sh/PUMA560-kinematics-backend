9edbcab58dbd5ee0ded52600845b2d820aeca1de
Purple-Code-sh <purple.code.sh@gmail.com>
2024-12-07 03:36:00 -0600

    Adds testing to T1 and T2 functions, and deletes unused code

--- teste.py
+++ teste.py
@@ -2,46 +2,6 @@ import sympy as sp
 from sympy.matrices import Matrix
 from sympy import *
 
-# #Matriz de transformación para la traslación en X, Y, Z
-# def Transl (x, y, z):
-#     return Matrix([
-#         [1, 0, 0, x],
-#         [0, 1, 0, y],
-#         [0, 0, 1, z],
-#         [0, 0, 0, 1]
-#     ])    
-
-# #Matriz de rotación en X para un angulo alfa
-# def RotX(alfa):
-#     return Matrix([
-#         [1, 0, 0, 0],
-#         [0, cos(alfa), -sin(alfa), 0],
-#         [0, sin(alfa), cos(alfa), 0],
-#         [0, 0, 0, 1]
-#     ])
-
-# #Matriz de rotación en Y para un angulo phi
-# def RotY(phi):
-#     return Matrix([
-#         [cos(phi), 0, sin(phi), 0],
-#         [0, 1, 0, 0],
-#         [-sin(phi), 0, cos(phi), 0],
-#         [0, 0, 0, 1]
-#     ])
-
-# #Matriz de rotación en Z para un angulo theta
-# def RotZ(theta):
-#     return Matrix([
-#         [cos(theta), -sin(theta), 0, 0],
-#         [sin(theta), cos(theta), 0, 0],
-#         [0, 0, 1, 0],
-#         [0, 0, 0, 1]
-#     ])
-
-# q, d, a, al = sp.symbols("q d a al")
-# A = RotZ(q) * Transl(0,0,d) * Transl(a,0,0) * RotX(al)
-# print(A)
-
 def A_01(ang1):
     return Matrix([
         [cos(ang1), 0, -sin(ang1), 0],
@@ -95,3 +55,13 @@ def T1(ang_1, ang_2, a_2, d_2, ang_3, a_3):
 
 def T2(ang_4, d_4, ang_5, ang_6, d_6):
     return A_34(ang_4, d_4) * A_45(ang_5) * A_56(ang_6, d_6)
+
+# # Testing values to T1
+# G1, G2, A2, D2, G3, A3= sp.symbols("G1 G2 A2 D2 G3 A3")
+# Mat_A = T1(G1, G2, A2, D2, G3, A3)
+# print(Mat_A)
+
+# # Testing values to T2
+# G4, D4, G5, G6, D6 = sp.symbols("g4 d4 g5 g6 d6")
+# Mat_B = T2(G4, D4, G5, G6, D6)
+# print(Mat_B)
