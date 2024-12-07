11072de7ca4ea1a6121bb724de1ae857cd274bd5
Purple-Code-sh <purple.code.sh@gmail.com>
2024-12-07 02:43:03 -0600

    Deletes unused code lines

--- teste.py
+++ teste.py
@@ -1,35 +1,6 @@
-import roboticstoolbox as rtb
-import numpy as np
 import sympy as sp
 from sympy.matrices import Matrix
 from sympy import *
-
-# robot = rtb.models.Panda()
-# print(robot)
-
-# x = np.array([1,2,3,4])
-# print(x)
-
-# b = x + 2
-# print(b)
-
-# c = np.sqrt(b)
-# print(c)
-
-# y = np.array([45, 60, 90, 180])
-# print(y)
-
-# grad = np.sin(y)
-# print(grad)
-
-# A = np.array([[1, 2],[3, 4]])
-# B = np.array([[5, 0],[-8, 3]])
-# print(A + B)
-
-# robot = rtb.models.Puma560()
-# print(robot)
-
-#_____________________________________________________________________________________________-
 
 #Matriz de transformación para la traslación en X, Y, Z
 def Transl (x, y, z):
@@ -67,5 +38,8 @@ def RotZ(theta):
         [0, 0, 0, 1]
     ])
 
-q, d, a, al = sp.symbols("q d a al")
-A = Rotz(q) * Transl(0,0,d) * Transl(a,0,0) * Rotx(al)
+# q, d, a, al = sp.symbols("q d a al")
+# A = RotZ(q) * Transl(0,0,d) * Transl(a,0,0) * RotX(al)
+# print(A)
+
+
