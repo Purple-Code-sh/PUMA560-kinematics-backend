ade7becf8458fe7881e7b107eff62f4d4307351e
Purple-Code-sh <purple.code.sh@gmail.com>
2024-12-07 02:37:05 -0600

    Creates transformation function

--- teste.py
+++ teste.py
@@ -1,6 +1,8 @@
 import roboticstoolbox as rtb
 import numpy as np
-
+import sympy as sp
+from sympy.matrices import Matrix
+from sympy import *
 
 # robot = rtb.models.Panda()
 # print(robot)
@@ -24,6 +26,46 @@ import numpy as np
 # B = np.array([[5, 0],[-8, 3]])
 # print(A + B)
 
-import roboticstoolbox as rtb
-robot = rtb.models.Puma560()
-print(robot)
+# robot = rtb.models.Puma560()
+# print(robot)
+
+#_____________________________________________________________________________________________-
+
+#Matriz de transformación para la traslación en X, Y, Z
+def Transl (x, y, z):
+    return Matrix([
+        [1, 0, 0, x],
+        [0, 1, 0, y],
+        [0, 0, 1, z],
+        [0, 0, 0, 1]
+    ])
+
+#Matriz de rotación en X para un angulo alfa
+def RotX(alfa):
+    return Matrix([
+        [1, 0, 0, 0],
+        [0, cos(alfa), -sin(alfa), 0],
+        [0, sin(alfa), cos(alfa), 0],
+        [0, 0, 0, 1]
+    ])
+
+#Matriz de rotación en Y para un angulo phi
+def RotY(phi):
+    return Matrix([
+        [cos(phi), 0, sin(phi), 0],
+        [0, 1, 0, 0],
+        [-sin(phi), 0, cos(phi), 0],
+        [0, 0, 0, 1]
+    ])
+
+#Matriz de rotación en Z para un angulo theta
+def RotZ(theta):
+    return Matrix([
+        [cos(theta), -sin(theta), 0, 0],
+        [sin(theta), cos(theta), 0, 0],
+        [0, 0, 1, 0],
+        [0, 0, 0, 1]
+    ])
+
+q, d, a, al = sp.symbols("q d a al")
+A = Rotz(q) * Transl(0,0,d) * Transl(a,0,0) * Rotx(al)
