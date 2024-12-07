5867d9a87eb53a5a7ae5b84502b2f5ac73b4cfed
Purple-Code-sh <purple.code.sh@gmail.com>
2024-12-07 04:04:31 -0600

    Resolves conflicts at matrices results, changing sin and cos functions tu use the provided by math library

--- teste.py
+++ teste.py
@@ -1,51 +1,50 @@
-import sympy as sp
+import math
 from sympy.matrices import Matrix
-from sympy import *
 
 def A_01(ang1):
     return Matrix([
-        [cos(ang1), 0, -sin(ang1), 0],
-        [sin(ang1), 0, cos(ang1), 0],
+        [math.cos(math.radians(ang1)), 0, -math.sin(math.radians(ang1)), 0],
+        [math.sin(math.radians(ang1)), 0, math.cos(math.radians(ang1)), 0],
         [0, -1, 0, 0],
         [0, 0, 0, 1]
     ])
 
-def A_12(ang2,a2, d2):
+def A_12(ang2, a2, d2):
     return Matrix([
-        [cos(ang2), -sin(ang2), 0, a2*cos(ang2)],
-        [sin(ang2), cos(ang2), 0, a2*sin(ang2)],
+        [math.cos(math.radians(ang2)), -math.sin(math.radians(ang2)), 0, a2 * math.cos(math.radians(ang2))],
+        [math.sin(math.radians(ang2)), math.cos(math.radians(ang2)), 0, a2 * math.sin(math.radians(ang2))],
         [0, 0, 1, d2],
         [0, 0, 0, 1]
     ])
 
 def A_23(ang3, a3):
     return Matrix([
-        [cos(ang3), 0, sin(ang3), a3*cos(ang3)],
-        [sin(ang3), 0, -cos(ang3), a3*sin(ang3)],
+        [math.cos(math.radians(ang3)), 0, math.sin(math.radians(ang3)), a3 * math.cos(math.radians(ang3))],
+        [math.sin(math.radians(ang3)), 0, -math.cos(math.radians(ang3)), a3 * math.sin(math.radians(ang3))],
         [0, 1, 0, 0],
         [0, 0, 0, 1]
     ])
 
 def A_34(ang4, d4):
     return Matrix([
-        [cos(ang4), 0, -sin(ang4), 0],
-        [sin(ang4), 0, cos(ang4), 0],
+        [math.cos(math.radians(ang4)), 0, -math.sin(math.radians(ang4)), 0],
+        [math.sin(math.radians(ang4)), 0, math.cos(math.radians(ang4)), 0],
         [0, -1, 0, d4],
         [0, 0, 0, 1]
     ])
 
 def A_45(ang5):
     return Matrix([
-        [cos(ang5), 0, sin(ang5), 0],
-        [sin(ang5), 0, -cos(ang5), 0],
+        [math.cos(math.radians(ang5)), 0, math.sin(math.radians(ang5)), 0],
+        [math.sin(math.radians(ang5)), 0, -math.cos(math.radians(ang5)), 0],
         [0, 1, 0, 0],
         [0, 0, 0, 1]
     ])
 
 def A_56(ang6, d6):
     return Matrix([
-        [cos(ang6), -sin(ang6), 0, 0],
-        [sin(ang6), cos(ang6), 0, 0],
+        [math.cos(math.radians(ang6)), -math.sin(math.radians(ang6)), 0, 0],
+        [math.sin(math.radians(ang6)), math.cos(math.radians(ang6)), 0, 0],
         [0, 0, 1, d6],
         [0, 0, 0, 1]
     ])
@@ -56,12 +55,20 @@ def T1(ang_1, ang_2, a_2, d_2, ang_3, a_3):
 def T2(ang_4, d_4, ang_5, ang_6, d_6):
     return A_34(ang_4, d_4) * A_45(ang_5) * A_56(ang_6, d_6)
 
-# # Testing values to T1
-# G1, G2, A2, D2, G3, A3= sp.symbols("G1 G2 A2 D2 G3 A3")
-# Mat_A = T1(G1, G2, A2, D2, G3, A3)
-# print(Mat_A)
+# Valores de prueba
+G1 = 90
+G2 = 0
+A2 = 431.80
+D2 = 149.09
+G3 = 90
+A3 = -20.32
+G4 = 0
+D4 = 433.07
+G5 = 0
+G6 = 0
+D6 = 56.25
 
-# # Testing values to T2
-# G4, D4, G5, G6, D6 = sp.symbols("g4 d4 g5 g6 d6")
-# Mat_B = T2(G4, D4, G5, G6, D6)
-# print(Mat_B)
+Mat_T1 = T1(G1, G2, A2, D2, G3, A3)
+Mat_T2 = T2(G4, D4, G5, G6, D6)
+Mat_T = Mat_T1 * Mat_T2
+print(Mat_T)
